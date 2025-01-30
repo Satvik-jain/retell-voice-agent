62d59d0ff94562d27f850e4fb2bbdf12c79af218
Satvik Jain <satvik.jain.kht@gmail.com>
2025-01-30 15:49:07 +0530

    env setup

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 from supabase import Client, create_client
+import os
 
 supabase_url = "https://vmlobhapeligtemjnuyo.supabase.co"
-supabase_key = "YOUR_SUPABASE_SERVICE_ROLE_KEY"
+supabase_key = os.getenv("YOUR_SUPABASE_SERVICE_ROLE_KEY")
 
 supabase = create_client(supabase_url, supabase_key)
+# print(supabase_key)
